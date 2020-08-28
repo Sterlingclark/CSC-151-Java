647db4f4763cb733718084cfe09d03bebf611f1d
Sterling <sterlingdclark13@gmail.com>
2020-08-28 17:48:03 -0400

    add java application project

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,19 @@
+//CSC 151 - Java Programming
+//Sterling Clark
+//8/25/2020
+package javaapplication2;
+
+/**
+ *
+ * @author sterl
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+}
