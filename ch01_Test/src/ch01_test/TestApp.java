b77e6b0595209fc4e68d37f5067d7b634d7d28d2
Sterling <sterlingdclark13@gmail.com>
2020-08-28 17:53:40 -0400

    Add chapter 1 files

---
+++ ch01_Test/src/ch01_test/TestApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//CSC 151 - Java Programming
+//Sterling Clark
+//8/25/2020
+package ch01_test;
+
+
+public class TestApp {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Success!");
+        System.out.println("Success!");
+        System.out.println("Success!");
+    }
+    
+}
