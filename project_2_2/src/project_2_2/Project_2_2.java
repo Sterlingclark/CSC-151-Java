c0229cdf38c818fd2fa8bc3a7262b4668c9e6eb9
Sterling Clark <40446170+Sterlingclark@users.noreply.github.com>
2020-08-31 11:10:36 -0400

    new project grade converter

---
+++ project_2_2/src/project_2_2/Project_2_2.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ */
+package project_2_2;
+
+/**
+ * CSC-151-Java Programing
+ * Sterling Clark
+ * 8/31/20
+ */
+public class Project_2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Welcome to the Grade Converter app
+        System.out.println("Welcome to the Grade Coverter Calculator");
+        System.out.println();
+        
+        // Create a Scanner object 
+        
+       
+    }
+    
+}
