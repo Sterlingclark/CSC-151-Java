f33d62052c392d0e386eca52f53eef7f71e576a8
Sterling Clark <40446170+Sterlingclark@users.noreply.github.com>
2020-08-31 13:28:38 -0400

    added scanner sc

--- project_2_2/src/project_2_2/Project_2_2.java
+++ project_2_2/src/project_2_2/Project_2_2.java
@@ -3,24 +3,27 @@
  */
 package project_2_2;
 
+import java.util.Scanner;
+
 /**
- * CSC-151-Java Programing
- * Sterling Clark
- * 8/31/20
- */
+    * CSC-151-Java Programing
+    * Sterling Clark
+    * 8/31/20
+    */
 public class Project_2_2 {
 
     /**
-     * @param args the command line arguments
-     */
+    @param args the command line arguments
+    */
     public static void main(String[] args) {
         // Welcome to the Grade Converter app
         System.out.println("Welcome to the Grade Coverter Calculator");
         System.out.println();
-        
+
         // Create a Scanner object 
+        Scanner sc = new Scanner(System.in);
+
         
-       
     }
     
 }
